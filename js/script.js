9851fcee08004850a8960d4ab2a36c840d38c059
LUEL Mulugeta TEGABU <127260520+zonefree41@users.noreply.github.com>
2023-03-21 18:02:12 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-alert('Hello world');
+//This creates the for loop to display the pokemon in the pages DOM.
+
+/* for (let i = 0; i < pokemonList.length; i++) {
+document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")<br>");
+} */
+
+// Commented out the original loop
+
+/* Created a new variable for the text that will be displayed "let pokemonText" and 
+used this to add a <p> element</p> to the display to help with styling
+used and if loop without the else statement to only dispplay the height related 
+text for a single pokemon. */
+
+for (let i = 0; i < pokemonList.length; i++) {
+    let pokemonText = '<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')';
+    if (pokemonList[i].height > 1.8) {
+        pokemonText += ' - wow that is big';
+    }
+    pokemonText += '</p>';
+    document.write(pokemonText);
+}
+
